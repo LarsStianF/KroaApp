5cbe1cc10b8cfd66389b527da5650a1b91bb3e01
BertBert0 <31742102+BertBert0@users.noreply.github.com>
2019-10-04 22:28:19 +0200

    Revert "Revert "Fuck if i know""
    This reverts commit 3a2fde88ece00f6327384ceca38d711cb89ae090.

--- app/src/main/java/com/example/kroasiden20/EventActivity.java
+++ app/src/main/java/com/example/kroasiden20/EventActivity.java
@@ -20,12 +20,28 @@ public class EventActivity extends AppCompatActivity {
         TextView evtStart = findViewById(R.id.startDetail);
         TextView evtEnd = findViewById(R.id.endDetail);
         TextView evtText = findViewById(R.id.textDetail);
+        TextView secNeed = findViewById(R.id.secDetailNeed);
+        TextView barNeed = findViewById(R.id.barDetailNeed);
+        TextView crwNeed = findViewById(R.id.crwDetailNeed);
+        TextView tchNeed = findViewById(R.id.tchDetailNeed);
+        TextView secGot = findViewById(R.id.secDetailGot);
+        TextView barGot = findViewById(R.id.barDetailGot);
+        TextView crwGot = findViewById(R.id.crwDetailGot);
+        TextView tchGot = findViewById(R.id.tchDetailGot);
 
         evtName.setText(getIntent().getStringExtra("Name"));
         evtDate.setText(getIntent().getStringExtra("Date"));
         evtStart.setText(getIntent().getStringExtra("Start"));
         evtEnd.setText(getIntent().getStringExtra("End"));
         evtText.setText(getIntent().getStringExtra("Txt"));
+        secNeed.setText(getIntent().getStringExtra("secNeed"));
+        barNeed.setText(getIntent().getStringExtra("barNeed"));
+        crwNeed.setText(getIntent().getStringExtra("crwNeed"));
+        tchNeed.setText(getIntent().getStringExtra("tchNeed"));
+        secGot.setText(getIntent().getStringExtra("secGot"));
+        barGot.setText(getIntent().getStringExtra("barGot"));
+        crwGot.setText(getIntent().getStringExtra("crwGot"));
+        tchGot.setText(getIntent().getStringExtra("tchGot"));
     }
 
 }
